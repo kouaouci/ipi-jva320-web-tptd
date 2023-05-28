c1b15be327cfd4adffdb260454674f49eebbc3d7
kouaouci <karimakouaouci2803@gmail.com>
2023-05-28 21:12:33 +0200

    pagination fini

--- src/main/java/com/ipi/jva320/controller/SalairieController.java
+++ src/main/java/com/ipi/jva320/controller/SalairieController.java
@@ -52,4 +52,47 @@ public class SalairieController {
         salairieService.deleteSalarieAideADomicile ( id );
         return "redirect:/home";
     }
+    /*
+    @RequestMapping("/salaries")
+    public String redirectToListWithParams(@RequestParam(required = false, name = "nom") String nom,
+                                           @RequestParam(required = false, name = "page", defaultValue = "1") int page,
+                                           @RequestParam(required = false, name = "size", defaultValue = "10") int size,
+                                           @RequestParam(required = false, name = "sortDirection", defaultValue = "desc") String sortDirection,
+                                           @RequestParam(required = false, name = "sortProperty", defaultValue = "id") String sortProperty,
+                                           final ModelMap model) {
+        Pageable pageable;
+        if (Objects.equals(sortDirection, "asc")) {
+            pageable = PageRequest.of(page, size, Sort.by(sortProperty).descending());
+        } else {
+            pageable = PageRequest.of(page, size, Sort.by(sortProperty).ascending());
+        }
+        if (nom == null || nom.length() <= 0) {
+            Page itemPages = salarieAideADomicileService.getSalaries(pageable);
+            model.addAttribute("salariees", itemPages.getContent());
+        } else {
+            model.addAttribute("salariees", salarieAideADomicileService.getSalaries(pageable));
+        }
+        model.put("page", page);
+        model.put("sortdir", sortDirection);
+        model.put("sortpro", sortProperty);
+        if(page == 0) {
+            model.put("disable", true);
+        } else {
+            model.put("disable", false);
+        }
+
+        return "list";
+    }*/
+
+  /*  @RequestMapping("/salaries/aide/new")
+    public String redirectToAdd(final ModelMap model) {
+        SalarieAideADomicile aide = new SalarieAideADomicile();
+        aide.setId(0L);
+        model.put("details", aide);
+        model.put("action", "save");
+        model.addAttribute("salarie", new SalarieAideADomicile());
+        return "detail_Salarie";
+    }*/
+
+
 }
